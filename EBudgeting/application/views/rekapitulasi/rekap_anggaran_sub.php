9fb88e586fbe74e0ea352a32e84434cae9e943c6
demaltn <89619742+demaltn@users.noreply.github.com>
2022-05-06 11:32:52 +0700

    Add files via upload

---
+++ EBudgeting/application/views/rekapitulasi/rekap_anggaran_sub.php
@@ -0,0 +1,264 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <title>E-Budgeting | Dashboard</title>
+    <!-- Tell the browser to be responsive to screen width -->
+    <meta content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no" name="viewport">
+    <!-- Bootstrap 3.3.7 -->
+    <link rel="stylesheet" href="<?php echo base_url() ?>assets/bower_components/bootstrap/dist/css/bootstrap.min.css">
+    <!-- Font Awesome -->
+    <link rel="stylesheet" href="<?php echo base_url() ?>assets/bower_components/font-awesome/css/font-awesome.min.css">
+    <!-- Ionicons -->
+    <link rel="stylesheet" href="<?php echo base_url() ?>assets/bower_components/Ionicons/css/ionicons.min.css">
+
+    <link rel="stylesheet" href="https://cdn.datatables.net/1.11.3/css/jquery.dataTables.min.css"> 
+    <link rel="stylesheet" href="https://cdn.datatables.net/buttons/2.0.1/css/buttons.dataTables.min.css">
+
+    <!-- Theme style -->
+    <link rel="stylesheet" href="<?php echo base_url() ?>assets/dist/css/AdminLTE.min.css">
+    <!-- AdminLTE Skins. Choose a skin from the css/skins
+       folder instead of downloading all of them to reduce the load. -->
+       <link rel="stylesheet" href="<?php echo base_url() ?>assets/dist/css/skins/_all-skins.min.css">
+       <!-- Morris chart -->
+       <link rel="stylesheet" href="<?php echo base_url() ?>assets/bower_components/morris.js/morris.css">
+       <!-- jvectormap -->
+       <link rel="stylesheet" href="<?php echo base_url() ?>assets/bower_components/jvectormap/jquery-jvectormap.css">
+       <!-- Date Picker -->
+       <link rel="stylesheet" href="<?php echo base_url() ?>assets/bower_components/bootstrap-datepicker/dist/css/bootstrap-datepicker.min.css">
+       <!-- Daterange picker -->
+       <link rel="stylesheet" href="<?php echo base_url() ?>assets/bower_components/bootstrap-daterangepicker/daterangepicker.css">
+       <!-- bootstrap wysihtml5 - text editor -->
+       <link rel="stylesheet" href="<?php echo base_url() ?>assets/plugins/bootstrap-wysihtml5/bootstrap3-wysihtml5.min.css">
+
+       <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
+       <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+    <!--[if lt IE 9]>
+  <script src="https://oss.maxcdn.com/html5shiv/3.7.3/html5shiv.min.js"></script>
+  <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+<![endif]-->
+
+<!-- Google Font -->
+<link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,600,700,300italic,400italic,600italic">
+</head>
+
+<body class="hold-transition skin-blue sidebar-mini">
+  <div class="wrapper">
+    <header class="main-header">
+      <!-- Logo -->
+      <a href="index2.html" class="logo">
+        <!-- mini logo for sidebar mini 50x50 pixels -->
+        <span class="logo-mini"><b>BDG</b></span>
+        <!-- logo for regular state and mobile devices -->
+        <span class="logo-lg"><b>E-Budgeting</b></span>
+    </a>
+    <!-- Header Navbar: style can be found in header.less -->
+    <nav class="navbar navbar-static-top">
+        <!-- Sidebar toggle button-->
+        <a href="#" class="sidebar-toggle" data-toggle="push-menu" role="button">
+          <span class="sr-only">Toggle navigation</span>
+      </a>
+
+      <div class="navbar-custom-menu">
+          <ul class="nav navbar-nav">
+
+            <!-- User Account: style can be found in dropdown.less -->
+            <li class="dropdown user user-menu">
+              <a href="#" class="dropdown-toggle" data-toggle="dropdown">
+                <img src="<?php echo base_url() ?>assets/dist/img/user2-160x160.jpg" class="user-image" alt="User Image">
+                <span class="hidden-xs">Sub Bidang</span>
+            </a>
+            <ul class="dropdown-menu">
+                <!-- User image -->
+                <li class="user-header">
+                  <img src="<?php echo base_url() ?>assets/dist/img/user2-160x160.jpg" class="img-circle" alt="User Image">
+
+                  <p>
+                    Account - Sub Bidang
+                </p>
+            </li>
+
+            <!-- Menu Footer-->
+            <li class="user-footer">
+              <div class="pull-left">
+                <a href="#" class="btn btn-default btn-flat">Profile</a>
+            </div>
+            <div class="pull-right">
+                <a href="logout_admin" class="btn btn-default btn-flat">Sign out</a>
+            </div>
+        </li>
+    </ul>
+</li>
+<!-- Control Sidebar Toggle Button -->
+<li>
+  <a href="#" data-toggle="control-sidebar"><i class="fa fa-gears"></i></a>
+</li>
+</ul>
+</div>
+</nav>
+</header>
+<!-- Left side column. contains the logo and sidebar -->
+<?php $this->load->view('dashboard/templatedmpau/sidebar'); ?>
+
+<!-- Content Wrapper. Contains page content -->
+<div class="content-wrapper">
+    <div class="container-fluid">
+        <!-- Content Header (Page header) -->
+        <section class="content-header">
+            <h1>
+                Rekapitulasi Anggaran
+            </h1>
+        </section>
+
+        <!-- Main content -->
+        <select name="bln" style="margin-bottom: 10px;margin-top: 10px;">
+            <option selected="selected">Bulan</option>
+            <?php
+            $bulan = array("Januari", "Februari", "Maret", "April", "Mei", "Juni", "Juli", "Agustus", "September", "Oktober", "November", "Desember");
+            $jlh_bln = count($bulan);
+            for ($c = 0; $c < $jlh_bln; $c += 1) {
+                echo "<option value=$bulan[$c]> $bulan[$c] </option>";
+            }
+            ?>
+        </select>
+        <div class="card mb-3">
+            <div class="card-body">
+
+                <div class="table-responsive">
+                    <table class="table table-bordered table-striped table-hover text-center" id="dataTable" width="100%" cellspacing="0">
+                        <thead>
+                            <tr>
+                                <th>No</th>
+                                <th>Kode Pos</th>
+                                <th>Uraian</th>
+                                <th>Rencana Kebutuhan (Minggu I)</th>
+                                <th>Rencana Kebutuhan (Minggu II)</th>
+                                <th>Rencana Kebutuhan (Minggu III)</th>
+                                <th>Rencana Kebutuhan (Minggu IV)</th>
+                                <th>Rencana Kebutuhan (Total)</th>
+                            </tr>
+                        </thead>
+                        <tbody class="table-striped">
+                            <tr>
+                                <!-- masukkan data dengan php echo beserta perulangan -->
+                                <td>1</td>
+                                <td>001</td>
+                                <td>Biaya Jasa</td>
+                                <td>2</td>
+                                <td>5</td>
+                                <td>1</td>
+                                <td>4</td>
+                                <td>12</td>
+                            </tr>
+                        </tbody>
+                    </table>
+                </div>
+            </div>
+        </div>
+    </div>
+
+
+    <!-- right col -->
+</div>
+<!-- /.row (main row) -->
+
+</section>
+<!-- /.content -->
+</div>
+
+    <!-- Add the sidebar's background. This div must be placed
+       immediately after the control sidebar -->
+       <div class="control-sidebar-bg"></div>
+   </div>
+   <!-- ./wrapper -->
+
+   <!-- jQuery 3 -->
+   <script src="<?php echo base_url() ?>assets/bower_components/jquery/dist/jquery.min.js"></script>
+   <!-- jQuery UI 1.11.4 -->
+   <script src="<?php echo base_url() ?>assets/bower_components/jquery-ui/jquery-ui.min.js"></script>
+   <!-- Resolve conflict in jQuery UI tooltip with Bootstrap tooltip -->
+   <script>
+    $.widget.bridge('uibutton', $.ui.button);
+</script>
+<!-- Bootstrap 3.3.7 -->
+<script src="<?php echo base_url() ?>assets/bower_components/bootstrap/dist/js/bootstrap.min.js"></script>
+<!-- Morris.js charts -->
+<script src="<?php echo base_url() ?>assets/bower_components/raphael/raphael.min.js"></script>
+<script src="<?php echo base_url() ?>assets/bower_components/morris.js/morris.min.js"></script>
+<!-- Sparkline -->
+<script src="<?php echo base_url() ?>assets/bower_components/jquery-sparkline/dist/jquery.sparkline.min.js"></script>
+<!-- jvectormap -->
+<script src="<?php echo base_url() ?>assets/plugins/jvectormap/jquery-jvectormap-1.2.2.min.js"></script>
+<script src="<?php echo base_url() ?>assets/plugins/jvectormap/jquery-jvectormap-world-mill-en.js"></script>
+<!-- jQuery Knob Chart -->
+<script src="<?php echo base_url() ?>assets/bower_components/jquery-knob/dist/jquery.knob.min.js"></script>
+<!-- daterangepicker -->
+<script src="<?php echo base_url() ?>assets/bower_components/moment/min/moment.min.js"></script>
+<script src="<?php echo base_url() ?>assets/bower_components/bootstrap-daterangepicker/daterangepicker.js"></script>
+<!-- datepicker -->
+<script src="<?php echo base_url() ?>assets/bower_components/bootstrap-datepicker/dist/js/bootstrap-datepicker.min.js"></script>
+<!-- Bootstrap WYSIHTML5 -->
+<script src="<?php echo base_url() ?>assets/plugins/bootstrap-wysihtml5/bootstrap3-wysihtml5.all.min.js"></script>
+
+<!-- <script type="text/javascript" language="javascript" src="https://code.jquery.com/jquery-3.5.1.js"></script> -->
+<script type="text/javascript" language="javascript" src="https://cdn.datatables.net/1.11.3/js/jquery.dataTables.min.js"></script>
+<script type="text/javascript" language="javascript" src="https://cdn.datatables.net/buttons/2.0.1/js/dataTables.buttons.min.js"></script>
+<script type="text/javascript" language="javascript" src="https://cdnjs.cloudflare.com/ajax/libs/jszip/3.1.3/jszip.min.js"></script>
+<script type="text/javascript" language="javascript" src="https://cdnjs.cloudflare.com/ajax/libs/pdfmake/0.1.53/pdfmake.min.js"></script>
+<script type="text/javascript" language="javascript" src="https://cdnjs.cloudflare.com/ajax/libs/pdfmake/0.1.53/vfs_fonts.js"></script>
+<script type="text/javascript" language="javascript" src="https://cdn.datatables.net/buttons/2.0.1/js/buttons.html5.min.js"></script>
+<script type="text/javascript" language="javascript" src="https://cdn.datatables.net/buttons/2.0.1/js/buttons.print.min.js"></script>
+
+
+<!-- Slimscroll -->
+<script src="<?php echo base_url() ?>assets/bower_components/jquery-slimscroll/jquery.slimscroll.min.js"></script>
+<!-- FastClick -->
+<script src="<?php echo base_url() ?>assets/bower_components/fastclick/lib/fastclick.js"></script>
+<!-- AdminLTE App -->
+<script src="<?php echo base_url() ?>assets/dist/js/adminlte.min.js"></script>
+<!-- AdminLTE dashboard demo (This is only for demo purposes) -->
+<script src="<?php echo base_url() ?>assets/dist/js/pages/dashboard.js"></script>
+<!-- AdminLTE for demo purposes -->
+<script src="<?php echo base_url() ?>assets/dist/js/demo.js"></script>
+
+<script>
+    $(document).ready(function() {
+        $('#dataTable').DataTable( {
+            dom: 'Bfrtip',
+            buttons: [
+            {
+                extend: 'copy',
+                title: 'Rekapitulasi Anggaran'
+
+            }, {
+                extend: 'csv',
+                title: 'Rekapitulasi Anggaran'
+
+            }, {
+                extend: 'pdf',
+                title: 'Rekapitulasi Anggaran'
+
+            }, {
+                extend: 'excel',
+                title: 'Rekapitulasi Anggaran'
+
+            }, { 
+               extend: 'print',
+               title: "Rekapitulasi Anggaran", 
+               customize: function ( win ) {
+                $(win.document.body).find('h1').css('text-align', 'center');
+                $(win.document.body).css( 'font-size', '16px' );
+                $(win.document.body).find( 'table' )
+                .addClass( 'compact' )
+                .css( 'font-size', 'inherit' );
+            }
+        }
+        ]
+    } );
+    } );
+</script>
+</body>
+
+</html>
