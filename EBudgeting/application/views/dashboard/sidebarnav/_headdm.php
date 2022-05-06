dab092e12be0b61ac88e537cb249b4edf34e1c8e
aaugesvina <89875584+augesvina@users.noreply.github.com>
2022-05-06 22:43:37 +0700

    update koreksi anggaran

--- EBudgeting/application/views/dashboard/sidebarnav/_headdm.php
+++ EBudgeting/application/views/dashboard/sidebarnav/_headdm.php
@@ -1,124 +1,124 @@
 <header class="main-header">
-      <!-- Logo -->
-      <a href="<?php echo site_url('C_login/login_admin'); ?>" class="logo">
-        <!-- mini logo for sidebar mini 50x50 pixels -->
-        <span class="logo-mini"><b>BDG</b></span>
-        <!-- logo for regular state and mobile devices -->
-        <span class="logo-lg"><b>E-Budgeting</b></span>
-      </a>
-      <!-- Header Navbar: style can be found in header.less -->
-      <nav class="navbar navbar-static-top">
-        <!-- Sidebar toggle button-->
-        <a href="#" class="sidebar-toggle" data-toggle="push-menu" role="button">
-          <span class="sr-only">Toggle navigation</span>
-        </a>
+  <!-- Logo -->
+  <a href="<?php echo site_url('C_login/login_admin'); ?>" class="logo">
+    <!-- mini logo for sidebar mini 50x50 pixels -->
+    <span class="logo-mini"><b>BDG</b></span>
+    <!-- logo for regular state and mobile devices -->
+    <span class="logo-lg"><b>E-Budgeting</b></span>
+  </a>
+  <!-- Header Navbar: style can be found in header.less -->
+  <nav class="navbar navbar-static-top">
+    <!-- Sidebar toggle button-->
+    <a href="#" class="sidebar-toggle" data-toggle="push-menu" role="button">
+      <span class="sr-only">Toggle navigation</span>
+    </a>
 
-        <div class="navbar-custom-menu">
-          <ul class="nav navbar-nav">
-            <!-- User Account: style can be found in dropdown.less -->
-            <li class="dropdown user user-menu">
-              <a href="#" class="dropdown-toggle" data-toggle="dropdown">
-                <img src="<?php echo base_url() ?>assets/dist/img/user2-160x160.jpg" class="user-image" alt="User Image">
-                <span class="hidden-xs"><?php echo $this->session->userdata('nama_anggota'); ?></span>
-              </a>
-              <ul class="dropdown-menu">
-                <!-- User image -->
-                <li class="user-header">
-                  <img src="<?php echo base_url() ?>assets/dist/img/user2-160x160.jpg" class="img-circle" alt="User Image">
+    <div class="navbar-custom-menu">
+      <ul class="nav navbar-nav">
+        <!-- User Account: style can be found in dropdown.less -->
+        <li class="dropdown user user-menu">
+          <a href="#" class="dropdown-toggle" data-toggle="dropdown">
+            <img src="<?php echo base_url() ?>assets/dist/img/user2-160x160.jpg" class="user-image" alt="User Image">
+            <span class="hidden-xs"><?php echo $this->session->userdata('nama_anggota'); ?></span>
+          </a>
+          <ul class="dropdown-menu">
+            <!-- User image -->
+            <li class="user-header">
+              <img src="<?php echo base_url() ?>assets/dist/img/user2-160x160.jpg" class="img-circle" alt="User Image">
 
-                  <p>
-                    Account - DM
-                  </p>
-                </li>
+              <p>
+                Account - DM
+              </p>
+            </li>
 
-                <!-- Menu Footer-->
-                <li class="user-footer">
-                  <!-- <div class="pull-left">
+            <!-- Menu Footer-->
+            <li class="user-footer">
+              <!-- <div class="pull-left">
                     <a href="#" class="btn btn-default btn-flat">Profile</a>
                   </div> -->
-                  <div class="pull-right">
-                    <a href="<?php echo site_url('C_login/logout_admin') ?>" class="btn btn-default btn-flat">Sign out</a>
-                  </div>
-                </li>
-              </ul>
+              <div class="pull-right">
+                <a href="<?php echo site_url('C_login/logout_admin') ?>" class="btn btn-default btn-flat">Sign out</a>
+              </div>
             </li>
-            <!-- Control Sidebar Toggle Button -->
           </ul>
-        </div>
-      </nav>
-    </header>
-    <!-- Left side column. contains the logo and sidebar -->
-    <aside class="main-sidebar">
-      <!-- sidebar: style can be found in sidebar.less -->
-      <section class="sidebar">
-        <!-- Sidebar user panel -->
-        <div class="user-panel">
-          <div class="pull-left image">
-            <img src="<?php echo base_url() ?>assets/dist/img/user2-160x160.jpg" class="img-circle" alt="User Image">
-          </div>
-          <div class="pull-left info">
-            <p>Welcome DM/ Bidang</p>
-            <a href="#"><i class="fa fa-circle text-success"></i> Online</a>
-          </div>
-        </div>
+        </li>
+        <!-- Control Sidebar Toggle Button -->
+      </ul>
+    </div>
+  </nav>
+</header>
+<!-- Left side column. contains the logo and sidebar -->
+<aside class="main-sidebar">
+  <!-- sidebar: style can be found in sidebar.less -->
+  <section class="sidebar">
+    <!-- Sidebar user panel -->
+    <div class="user-panel">
+      <div class="pull-left image">
+        <img src="<?php echo base_url() ?>assets/dist/img/user2-160x160.jpg" class="img-circle" alt="User Image">
+      </div>
+      <div class="pull-left info">
+        <p>Welcome DM/ Bidang</p>
+        <a href="#"><i class="fa fa-circle text-success"></i> Online</a>
+      </div>
+    </div>
 
-        <!-- sidebar menu: : style can be found in sidebar.less -->
-        <ul class="sidebar-menu" data-widget="tree">
-          <li class="header">MAIN NAVIGATION</li>
-          <li class="active treeview">
-            <a href="#">
-              <i class="fa fa-dashboard"></i> <span>Data Master</span>
-              <span class="pull-right-container">
-                <i class="fa fa-angle-left pull-right"></i>
-              </span>
-            </a>
-            <ul class="treeview-menu">
-              <li class="active"><a href="<?php echo site_url("C_masterpos_subpos/show_pos"); ?>"><i class="fa fa-circle-o"></i> Pos</a></li>
-              <li><a href="<?php echo site_url("C_masterpos_subpos/show_subpos"); ?>"><i class="fa fa-circle-o"></i> Sub Pos</a></li>
-              <li><a href="<?php echo site_url("C_masterpos_subpos/show_subpos2"); ?>"><i class="fa fa-circle-o"></i> Sub Pos Barang </a></li>
-            </ul>
-          </li>
-          <li class=" active treeview">
-            <a href="#">
-              <i class="fa fa-files-o"></i> <span>Rekapitulasi</span>
-              <span class="pull-right-container">
-                <i class="fa fa-angle-left pull-right"></i>
-              </span>
-            </a>
-            <ul class="treeview-menu">
-              <li><a href="<?php echo site_url("C_rekap_pos"); ?>"><i class="fa fa-circle-o"></i> Rekap Pos Anggaran</a></li>
-              <li><a href="<?php echo site_url("C_rekap_anggaran"); ?>"><i class="fa fa-circle-o"></i> Rekap Anggaran </a></li>
-            </ul>
-          </li>
-          <li>
-          <li><a href="<?php echo site_url("C_persetujuan_dm"); ?>"><i class="fa fa-check"></i> <span>Persetujuan DM</span>
-              <span class="pull-right-container">
-                <span class="pull-right-container">
-                </span>
-            </a>
-          </li>
-          <li>
-            <a href="#">
-              <i class="fa fa-th"></i> <span>Koreksi Anggaran</span>
-              <span class="pull-right-container">
-              </span>
-            </a>
-          </li>
-          <li>
-            <a href="<?php echo site_url("C_menutransfer"); ?>">
-              <i class="fa fa-edit"></i> <span>Transfer</span>
-              <span class="pull-right-container">
-              </span>
-            </a>
-          </li>
-          <li>
-            <a href="<?php echo site_url("C_paguanggaran"); ?>">
-              <i class="fa fa-laptop"></i> <span>Setting Pagu Anggaran</span>
-              <span class="pull-right-container">
-              </span>
-            </a>
+    <!-- sidebar menu: : style can be found in sidebar.less -->
+    <ul class="sidebar-menu" data-widget="tree">
+      <li class="header">MAIN NAVIGATION</li>
+      <li class="active treeview">
+        <a href="#">
+          <i class="fa fa-dashboard"></i> <span>Data Master</span>
+          <span class="pull-right-container">
+            <i class="fa fa-angle-left pull-right"></i>
+          </span>
+        </a>
+        <ul class="treeview-menu">
+          <li class="active"><a href="<?php echo site_url("C_masterpos_subpos/show_pos"); ?>"><i class="fa fa-circle-o"></i> Pos</a></li>
+          <li><a href="<?php echo site_url("C_masterpos_subpos/show_subpos"); ?>"><i class="fa fa-circle-o"></i> Sub Pos</a></li>
+          <li><a href="<?php echo site_url("C_masterpos_subpos/show_subpos2"); ?>"><i class="fa fa-circle-o"></i> Sub Pos Barang </a></li>
+        </ul>
+      </li>
+      <li class=" active treeview">
+        <a href="#">
+          <i class="fa fa-files-o"></i> <span>Rekapitulasi</span>
+          <span class="pull-right-container">
+            <i class="fa fa-angle-left pull-right"></i>
+          </span>
+        </a>
+        <ul class="treeview-menu">
+          <li><a href="<?php echo site_url("C_rekap_pos"); ?>"><i class="fa fa-circle-o"></i> Rekap Pos Anggaran</a></li>
+          <li><a href="<?php echo site_url("C_rekap_anggaran"); ?>"><i class="fa fa-circle-o"></i> Rekap Anggaran </a></li>
+        </ul>
+      </li>
+      <li>
+      <li><a href="<?php echo site_url("C_persetujuan_dm"); ?>"><i class="fa fa-check"></i> <span>Persetujuan DM</span>
+          <span class="pull-right-container">
+            <span class="pull-right-container">
+            </span>
+        </a>
+      </li>
+      <li>
+        <a href="<?php echo site_url("C_koreksi_anggaran"); ?>">
+          <i class="fa fa-th"></i> <span>Koreksi Anggaran</span>
+          <span class="pull-right-container">
+          </span>
+        </a>
+      </li>
+      <li>
+        <a href="<?php echo site_url("C_menutransfer"); ?>">
+          <i class="fa fa-edit"></i> <span>Transfer</span>
+          <span class="pull-right-container">
+          </span>
+        </a>
+      </li>
+      <li>
+        <a href="<?php echo site_url("C_paguanggaran"); ?>">
+          <i class="fa fa-laptop"></i> <span>Setting Pagu Anggaran</span>
+          <span class="pull-right-container">
+          </span>
+        </a>
 
-          </li>
-      </section>
-      <!-- /.sidebar -->
-    </aside>
+      </li>
+  </section>
+  <!-- /.sidebar -->
+</aside>
--- EBudgeting/application/views/dashboard/sidebarnav/_headdmpau.php
+++ EBudgeting/application/views/dashboard/sidebarnav/_headdmpau.php
@@ -101,7 +101,7 @@
         </a>
       </li>
       <li>
-        <a href="pages/widgets.html">
+        <a href="<?php echo site_url("C_koreksi_anggaran"); ?>">
           <i class="fa fa-th"></i> <span>Koreksi Anggaran</span>
           <span class="pull-right-container">
           </span>
