d08ec52e63c44a69cc65275a58d5d69413bc7bf0
Intan Naumi <intanaomi28@student.uns.ac.id>
2022-05-07 20:54:25 +0700

    update rekap all

--- EBudgeting/application/views/rekapitulasi/rekap_anggaran.php
+++ EBudgeting/application/views/rekapitulasi/rekap_anggaran.php
@@ -84,150 +84,154 @@
                 </section>
 
                 <!-- Main content -->
-                <select name="bln" style="margin-bottom: 10px;margin-top: 10px;">
-                    <option selected="selected">Bulan</option>
-                    <?php
-                    $bulan = array("Januari", "Februari", "Maret", "April", "Mei", "Juni", "Juli", "Agustus", "September", "Oktober", "November", "Desember");
-                    $jlh_bln = count($bulan);
-                    for ($c = 0; $c < $jlh_bln; $c += 1) {
-                        echo "<option value=$bulan[$c]> $bulan[$c] </option>";
-                    }
-                    ?>
-                </select>
-                <div class="card mb-3">
-                    <div class="card-body">
+                <section class="content">
+                    <div class="row">
 
-                        <div class="table-responsive">
-                            <table class="table table-bordered table-striped table-hover text-center" id="dataTable" width="100%" cellspacing="0">
-                                <thead>
-                                    <tr>
-                                        <th>No</th>
-                                        <th>Kode Pos</th>
-                                        <th>Uraian</th>
-                                        <th>Rencana Kebutuhan (Minggu I)</th>
-                                        <th>Rencana Kebutuhan (Minggu II)</th>
-                                        <th>Rencana Kebutuhan (Minggu III)</th>
-                                        <th>Rencana Kebutuhan (Minggu IV)</th>
-                                        <th>Rencana Kebutuhan (Total)</th>
-                                    </tr>
-                                </thead>
-                                <tbody class="table-striped">
-                                    <tr>
-                                        <!-- masukkan data dengan php echo beserta perulangan -->
-                                        <td>1</td>
-                                        <td>001</td>
-                                        <td>Biaya Jasa</td>
-                                        <td>2</td>
-                                        <td>5</td>
-                                        <td>1</td>
-                                        <td>4</td>
-                                        <td>12</td>
-                                    </tr>
-                                </tbody>
-                            </table>
+                        <div class="col-md-12">
+                            <select name="bln" style="margin-bottom: 10px;margin-top: 10px;">
+                                <option selected="selected">Bulan</option>
+                                <?php
+                                $bulan = array("Januari", "Februari", "Maret", "April", "Mei", "Juni", "Juli", "Agustus", "September", "Oktober", "November", "Desember");
+                                $jlh_bln = count($bulan);
+                                for ($c = 0; $c < $jlh_bln; $c += 1) {
+                                    echo "<option value=$bulan[$c]> $bulan[$c] </option>";
+                                }
+                                ?>
+                            </select>
+                            <div class="card mb-3">
+                                <div class="card-body">
+
+                                    <div class="table-responsive">
+                                        <table class="table table-bordered table-striped table-hover text-center" id="dataTable" width="100%" cellspacing="0">
+                                            <thead>
+                                                <tr>
+                                                    <th>No</th>
+                                                    <th>Kode Pos</th>
+                                                    <th>Uraian</th>
+                                                    <th>Rencana Kebutuhan (Minggu I)</th>
+                                                    <th>Rencana Kebutuhan (Minggu II)</th>
+                                                    <th>Rencana Kebutuhan (Minggu III)</th>
+                                                    <th>Rencana Kebutuhan (Minggu IV)</th>
+                                                    <th>Rencana Kebutuhan (Total)</th>
+                                                </tr>
+                                            </thead>
+                                            <tbody class="table-striped">
+                                                <tr>
+                                                    <!-- masukkan data dengan php echo beserta perulangan -->
+                                                    <td>1</td>
+                                                    <td>001</td>
+                                                    <td>Biaya Jasa</td>
+                                                    <td>2</td>
+                                                    <td>5</td>
+                                                    <td>1</td>
+                                                    <td>4</td>
+                                                    <td>12</td>
+                                                </tr>
+                                            </tbody>
+                                        </table>
+                                    </div>
+                                </div>
+                            </div>
                         </div>
+
+
+                        <!-- right col -->
                     </div>
-                </div>
+                    <!-- /.row (main row) -->
+
+                </section>
+                <!-- /.content -->
             </div>
 
-
-            <!-- right col -->
-        </div>
-        <!-- /.row (main row) -->
-
-        </section>
-        <!-- /.content -->
-    </div>
-
-    <!-- Add the sidebar's background. This div must be placed
+            <!-- Add the sidebar's background. This div must be placed
        immediately after the control sidebar -->
-    <div class="control-sidebar-bg"></div>
-    </div>
-    <!-- ./wrapper -->
+            <div class="control-sidebar-bg"></div>
+        </div>
+        <!-- ./wrapper -->
 
-    <!-- jQuery 3 -->
-    <script src="<?php echo base_url() ?>assets/bower_components/jquery/dist/jquery.min.js"></script>
-    <!-- jQuery UI 1.11.4 -->
-    <script src="<?php echo base_url() ?>assets/bower_components/jquery-ui/jquery-ui.min.js"></script>
-    <!-- Resolve conflict in jQuery UI tooltip with Bootstrap tooltip -->
-    <script>
-        $.widget.bridge('uibutton', $.ui.button);
-    </script>
-    <!-- Bootstrap 3.3.7 -->
-    <script src="<?php echo base_url() ?>assets/bower_components/bootstrap/dist/js/bootstrap.min.js"></script>
-    <!-- Morris.js charts -->
-    <script src="<?php echo base_url() ?>assets/bower_components/raphael/raphael.min.js"></script>
-    <script src="<?php echo base_url() ?>assets/bower_components/morris.js/morris.min.js"></script>
-    <!-- Sparkline -->
-    <script src="<?php echo base_url() ?>assets/bower_components/jquery-sparkline/dist/jquery.sparkline.min.js"></script>
-    <!-- jvectormap -->
-    <script src="<?php echo base_url() ?>assets/plugins/jvectormap/jquery-jvectormap-1.2.2.min.js"></script>
-    <script src="<?php echo base_url() ?>assets/plugins/jvectormap/jquery-jvectormap-world-mill-en.js"></script>
-    <!-- jQuery Knob Chart -->
-    <script src="<?php echo base_url() ?>assets/bower_components/jquery-knob/dist/jquery.knob.min.js"></script>
-    <!-- daterangepicker -->
-    <script src="<?php echo base_url() ?>assets/bower_components/moment/min/moment.min.js"></script>
-    <script src="<?php echo base_url() ?>assets/bower_components/bootstrap-daterangepicker/daterangepicker.js"></script>
-    <!-- datepicker -->
-    <script src="<?php echo base_url() ?>assets/bower_components/bootstrap-datepicker/dist/js/bootstrap-datepicker.min.js"></script>
-    <!-- Bootstrap WYSIHTML5 -->
-    <script src="<?php echo base_url() ?>assets/plugins/bootstrap-wysihtml5/bootstrap3-wysihtml5.all.min.js"></script>
+        <!-- jQuery 3 -->
+        <script src="<?php echo base_url() ?>assets/bower_components/jquery/dist/jquery.min.js"></script>
+        <!-- jQuery UI 1.11.4 -->
+        <script src="<?php echo base_url() ?>assets/bower_components/jquery-ui/jquery-ui.min.js"></script>
+        <!-- Resolve conflict in jQuery UI tooltip with Bootstrap tooltip -->
+        <script>
+            $.widget.bridge('uibutton', $.ui.button);
+        </script>
+        <!-- Bootstrap 3.3.7 -->
+        <script src="<?php echo base_url() ?>assets/bower_components/bootstrap/dist/js/bootstrap.min.js"></script>
+        <!-- Morris.js charts -->
+        <script src="<?php echo base_url() ?>assets/bower_components/raphael/raphael.min.js"></script>
+        <script src="<?php echo base_url() ?>assets/bower_components/morris.js/morris.min.js"></script>
+        <!-- Sparkline -->
+        <script src="<?php echo base_url() ?>assets/bower_components/jquery-sparkline/dist/jquery.sparkline.min.js"></script>
+        <!-- jvectormap -->
+        <script src="<?php echo base_url() ?>assets/plugins/jvectormap/jquery-jvectormap-1.2.2.min.js"></script>
+        <script src="<?php echo base_url() ?>assets/plugins/jvectormap/jquery-jvectormap-world-mill-en.js"></script>
+        <!-- jQuery Knob Chart -->
+        <script src="<?php echo base_url() ?>assets/bower_components/jquery-knob/dist/jquery.knob.min.js"></script>
+        <!-- daterangepicker -->
+        <script src="<?php echo base_url() ?>assets/bower_components/moment/min/moment.min.js"></script>
+        <script src="<?php echo base_url() ?>assets/bower_components/bootstrap-daterangepicker/daterangepicker.js"></script>
+        <!-- datepicker -->
+        <script src="<?php echo base_url() ?>assets/bower_components/bootstrap-datepicker/dist/js/bootstrap-datepicker.min.js"></script>
+        <!-- Bootstrap WYSIHTML5 -->
+        <script src="<?php echo base_url() ?>assets/plugins/bootstrap-wysihtml5/bootstrap3-wysihtml5.all.min.js"></script>
 
-    <!-- <script type="text/javascript" language="javascript" src="https://code.jquery.com/jquery-3.5.1.js"></script> -->
-    <script type="text/javascript" language="javascript" src="https://cdn.datatables.net/1.11.3/js/jquery.dataTables.min.js"></script>
-    <script type="text/javascript" language="javascript" src="https://cdn.datatables.net/buttons/2.0.1/js/dataTables.buttons.min.js"></script>
-    <script type="text/javascript" language="javascript" src="https://cdnjs.cloudflare.com/ajax/libs/jszip/3.1.3/jszip.min.js"></script>
-    <script type="text/javascript" language="javascript" src="https://cdnjs.cloudflare.com/ajax/libs/pdfmake/0.1.53/pdfmake.min.js"></script>
-    <script type="text/javascript" language="javascript" src="https://cdnjs.cloudflare.com/ajax/libs/pdfmake/0.1.53/vfs_fonts.js"></script>
-    <script type="text/javascript" language="javascript" src="https://cdn.datatables.net/buttons/2.0.1/js/buttons.html5.min.js"></script>
-    <script type="text/javascript" language="javascript" src="https://cdn.datatables.net/buttons/2.0.1/js/buttons.print.min.js"></script>
+        <!-- <script type="text/javascript" language="javascript" src="https://code.jquery.com/jquery-3.5.1.js"></script> -->
+        <script type="text/javascript" language="javascript" src="https://cdn.datatables.net/1.11.3/js/jquery.dataTables.min.js"></script>
+        <script type="text/javascript" language="javascript" src="https://cdn.datatables.net/buttons/2.0.1/js/dataTables.buttons.min.js"></script>
+        <script type="text/javascript" language="javascript" src="https://cdnjs.cloudflare.com/ajax/libs/jszip/3.1.3/jszip.min.js"></script>
+        <script type="text/javascript" language="javascript" src="https://cdnjs.cloudflare.com/ajax/libs/pdfmake/0.1.53/pdfmake.min.js"></script>
+        <script type="text/javascript" language="javascript" src="https://cdnjs.cloudflare.com/ajax/libs/pdfmake/0.1.53/vfs_fonts.js"></script>
+        <script type="text/javascript" language="javascript" src="https://cdn.datatables.net/buttons/2.0.1/js/buttons.html5.min.js"></script>
+        <script type="text/javascript" language="javascript" src="https://cdn.datatables.net/buttons/2.0.1/js/buttons.print.min.js"></script>
 
 
-    <!-- Slimscroll -->
-    <script src="<?php echo base_url() ?>assets/bower_components/jquery-slimscroll/jquery.slimscroll.min.js"></script>
-    <!-- FastClick -->
-    <script src="<?php echo base_url() ?>assets/bower_components/fastclick/lib/fastclick.js"></script>
-    <!-- AdminLTE App -->
-    <script src="<?php echo base_url() ?>assets/dist/js/adminlte.min.js"></script>
-    <!-- AdminLTE dashboard demo (This is only for demo purposes) -->
-    <script src="<?php echo base_url() ?>assets/dist/js/pages/dashboard.js"></script>
-    <!-- AdminLTE for demo purposes -->
-    <script src="<?php echo base_url() ?>assets/dist/js/demo.js"></script>
+        <!-- Slimscroll -->
+        <script src="<?php echo base_url() ?>assets/bower_components/jquery-slimscroll/jquery.slimscroll.min.js"></script>
+        <!-- FastClick -->
+        <script src="<?php echo base_url() ?>assets/bower_components/fastclick/lib/fastclick.js"></script>
+        <!-- AdminLTE App -->
+        <script src="<?php echo base_url() ?>assets/dist/js/adminlte.min.js"></script>
+        <!-- AdminLTE dashboard demo (This is only for demo purposes) -->
+        <script src="<?php echo base_url() ?>assets/dist/js/pages/dashboard.js"></script>
+        <!-- AdminLTE for demo purposes -->
+        <script src="<?php echo base_url() ?>assets/dist/js/demo.js"></script>
 
-    <script>
-        $(document).ready(function() {
-            $('#dataTable').DataTable({
-                dom: 'Bfrtip',
-                buttons: [{
-                    extend: 'copy',
-                    title: 'Rekapitulasi Anggaran'
+        <script>
+            $(document).ready(function() {
+                $('#dataTable').DataTable({
+                    dom: 'Bfrtip',
+                    buttons: [{
+                        extend: 'copy',
+                        title: 'Rekapitulasi Anggaran'
 
-                }, {
-                    extend: 'csv',
-                    title: 'Rekapitulasi Anggaran'
+                    }, {
+                        extend: 'csv',
+                        title: 'Rekapitulasi Anggaran'
 
-                }, {
-                    extend: 'pdf',
-                    title: 'Rekapitulasi Anggaran'
+                    }, {
+                        extend: 'pdf',
+                        title: 'Rekapitulasi Anggaran'
 
-                }, {
-                    extend: 'excel',
-                    title: 'Rekapitulasi Anggaran'
+                    }, {
+                        extend: 'excel',
+                        title: 'Rekapitulasi Anggaran'
 
-                }, {
-                    extend: 'print',
-                    title: "Rekapitulasi Anggaran",
-                    customize: function(win) {
-                        $(win.document.body).find('h1').css('text-align', 'center');
-                        $(win.document.body).css('font-size', '16px');
-                        $(win.document.body).find('table')
-                            .addClass('compact')
-                            .css('font-size', 'inherit');
-                    }
-                }]
+                    }, {
+                        extend: 'print',
+                        title: "Rekapitulasi Anggaran",
+                        customize: function(win) {
+                            $(win.document.body).find('h1').css('text-align', 'center');
+                            $(win.document.body).css('font-size', '16px');
+                            $(win.document.body).find('table')
+                                .addClass('compact')
+                                .css('font-size', 'inherit');
+                        }
+                    }]
+                });
             });
-        });
-    </script>
+        </script>
 </body>
 
 </html>
