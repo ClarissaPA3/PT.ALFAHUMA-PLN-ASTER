ffa348591c237b50dec003e033b4a27522f599ee
Clarissa Putri Aurellia <cputriaurellia@gmail.com>
2022-05-23 21:23:49 +0700

    update

--- EBudgeting/application/views/dashboard/dashboard_bidang.php
+++ EBudgeting/application/views/dashboard/dashboard_bidang.php
@@ -158,7 +158,7 @@
               <div class="icon">
                 <i class="fa fa-book"></i>
               </div>
-              <a href="#" class="small-box-footer">More info <i class="fa fa-arrow-circle-right"></i></a>
+              
             </div>
           </div>
           <!-- ./col -->
@@ -173,7 +173,7 @@
               <div class="icon">
                 <i class="ion ion-stats-bars"></i>
               </div>
-              <a href="#" class="small-box-footer">More info <i class="fa fa-arrow-circle-right"></i></a>
+              
             </div>
           </div>
           <!-- ./col -->
@@ -188,7 +188,6 @@
               <div class="icon">
                 <i class="fa fa-edit"></i>
               </div>
-              <a href="#" class="small-box-footer">More info <i class="fa fa-arrow-circle-right"></i></a>
             </div>
           </div>
           <!-- ./col -->
@@ -203,7 +202,6 @@
               <div class="icon">
                 <i class="fa fa-list"></i>
               </div>
-              <a href="#" class="small-box-footer">More info <i class="fa fa-arrow-circle-right"></i></a>
             </div>
           </div>
           <!-- ./col -->
@@ -231,42 +229,7 @@
 
           </section>
           <!-- /.Left col -->
-          <!-- right col (We are only adding the ID to make the widgets sortable)-->
-          <section class="col-lg-5 connectedSortable">
-
-            <!-- Calendar -->
-            <div class="box box-solid bg-green-gradient">
-              <div class="box-header">
-                <i class="fa fa-calendar"></i>
-
-                <h3 class="box-title">Calendar</h3>
-                <!-- tools box -->
-                <div class="pull-right box-tools">
-                  <!-- button with a dropdown -->
-                  <div class="btn-group">
-                    <button type="button" class="btn btn-success btn-sm dropdown-toggle" data-toggle="dropdown">
-                      <i class="fa fa-bars"></i></button>
-                    <ul class="dropdown-menu pull-right" role="menu">
-                      <li><a href="#">Add new event</a></li>
-                      <li><a href="#">Clear events</a></li>
-                      <li class="divider"></li>
-                      <li><a href="#">View calendar</a></li>
-                    </ul>
-                  </div>
-                  <button type="button" class="btn btn-success btn-sm" data-widget="collapse"><i class="fa fa-minus"></i>
-                  </button>
-                  <button type="button" class="btn btn-success btn-sm" data-widget="remove"><i class="fa fa-times"></i>
-                  </button>
-                </div>
-                <!-- /. tools -->
-              </div>
-              <!-- /.box-header -->
-              <div class="box-body no-padding">
-                <!--The calendar -->
-                <div id="calendar" style="width: 100%"></div>
-              </div>
-          </section>
-          <!-- right col -->
+          
         </div>
         <!-- /.row (main row) -->
 
